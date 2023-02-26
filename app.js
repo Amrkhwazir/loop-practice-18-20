03b5cc7f184ac2917fbf4332f8f69104d14fbb9f
sanaHamid123 <sidrahanifmhanif@gmail.com>
2023-02-27 00:31:04 +0500

    loop practice

---
+++ app.js
@@ -0,0 +1,74 @@
+// var fruis = ["Apple","Orange","banana","mango","pineapple","strawberry","graps"]
+
+// for(let a = 0; a <= 6; a++){
+//     console.log(fruis[a])
+// }
+
+// var table = prompt("Enter any number");
+// var heading = "Table"
+// document.write("<h1> Table </h1> <br>")
+
+// for(let a = 10; a >= 1; a--){
+
+//    console.log(a);
+
+// }
+
+// for(let a = 1; a <= 15; a++){
+
+//    if(a % 2 == 0){
+//       console.log(a);
+//    }
+
+// }
+
+// for(let a = 1; a <= 15; a++){
+
+//    if(a % 2 !== 0){
+//       console.log(a);
+//    }
+
+// }
+
+// var numOfSEries = ["2k","4k","6k","8k","10k","12k","14k","16k","18k","20k"]
+// for(let a=0; a < numOfSEries.length; a++){
+//    console.log(numOfSEries[a]);
+// }
+
+// var A = [24,53,86,38,22,55];
+// var CaptialNumber = A[0];
+// for(let a = 0; a < A.length; a++){
+//    if(A[a] > CaptialNumber ){
+//       CaptialNumber = A[a];
+//    }
+// }
+
+// console.log(CaptialNumber)
+
+// var A = [24,53,86,38,22,55];
+// var CaptialNumber = A[0];
+// for(let a = 0; a < A.length; a++){
+//    if(A[a] < CaptialNumber ){
+//       CaptialNumber = A[a];
+//    }
+// }
+
+// console.log(CaptialNumber)
+
+// var range = 5;
+// for(let a = 1; a <= 20; a++){
+//    console.log(range*a)
+// }
+
+// var students = ["ali","amir","furqan","asad"]
+// var score = [55,60,70,45];
+
+// for(let a = 0; a < students.length; a++){
+//    console.log(students[a],score[a])
+// }
+
+// var score2 = [12, 45, 23, 54, 28];
+// var userInput = prompt("kch b");
+// for (let a = 0; a < score2.length && score2[a] <= userInput; a++) {
+//   console.log(score2[a]);
+// }
